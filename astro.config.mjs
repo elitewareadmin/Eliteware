cd38ecfdf619dc8cc7686ba225a50b64f1dfb84c
jarvishuice <jarvis.realg@gmail.com>
2025-06-18 10:03:31 -0400

    feat: Añadir configuración de despliegue con gh-pages y actualizar dependencias

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
-import react from '@astrojs/react';
+import { defineConfig } from "astro/config";
+import tailwind from "@astrojs/tailwind";
+import react from "@astrojs/react";
+import { output } from "three/examples/jsm/nodes/Nodes.js";
 
 // https://astro.build/config
-export default defineConfig({integrations: [
-    tailwind(),
-    react(),
-    
-  ],site:''});
+export default defineConfig({
+  integrations: [tailwind(), react()],
+  output: "static",
+  site: "https://elitewareadmin.github.io",
+});
